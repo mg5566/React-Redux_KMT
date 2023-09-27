0f97bb5172e2a00e95ce59d7351f2a3371bc5c29
Mike <mike.kangaroo.world@gmail.com>
2023-09-27 14:09:52 +0900

    [feat] create app.js
    element 를 만들어내는 function 구현

---
+++ Part_1/app.js
@@ -0,0 +1,18 @@
+function createElement(type, props) {
+  switch (type) {
+    case "h1":
+      return [document.createElement("h1")].map((element) => {
+        Object.entries({ ...props, "data-id": "title" }).forEach(
+          ([name, value]) => element.setAttribute(name, value)
+        );
+        return element;
+      })[0];
+    case "div":
+      return [document.createElement("div")].map((element) => {
+        Object.entries({ ...props, "data-id": "layout" }).forEach(
+          ([name, value]) => element.setAttribute(name, value)
+        );
+        return element;
+      });
+  }
+}
