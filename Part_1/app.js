2d0c1b5da18e4a17a4fe6a4755f8539949dcc52c
Mike <mike.kangaroo.world@gmail.com>
2023-09-27 15:42:31 +0900

    [ref] refactor to map
    switch case 문을 mapping 하여 사용하는 방식으로 개선

--- Part_1/app.js
+++ Part_1/app.js
@@ -16,21 +16,11 @@ function createDiv(props) {
   });
 }
 
+const creatorMap = {
+  h1: createH1,
+  div: createDiv,
+};
+
 function createElement(type, props) {
-  switch (type) {
-    case "h1":
-      return [document.createElement("h1")].map((element) => {
-        Object.entries({ ...props, "data-id": "title" }).forEach(
-          ([name, value]) => element.setAttribute(name, value)
-        );
-        return element;
-      })[0];
-    case "div":
-      return [document.createElement("div")].map((element) => {
-        Object.entries({ ...props, "data-id": "layout" }).forEach(
-          ([name, value]) => element.setAttribute(name, value)
-        );
-        return element;
-      });
-  }
+  return creatorMap[type](props);
 }
