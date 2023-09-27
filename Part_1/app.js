a72d73892f7d6f63dc1f7717befa47ae23b0ff8e
Mike <mike.kangaroo.world@gmail.com>
2023-09-27 15:41:07 +0900

    [ref] refactor to modularization
    case 로직을 모듈화

--- Part_1/app.js
+++ Part_1/app.js
@@ -1,3 +1,21 @@
+function createH1(props) {
+  return [document.createElement("h1")].map((element) => {
+    Object.entries({ ...props, "data-id": "title" }).forEach(([name, value]) =>
+      element.setAttribute(name, value)
+    );
+    return element;
+  })[0];
+}
+
+function createDiv(props) {
+  return [document.createElement("div")].map((element) => {
+    Object.entries({ ...props, "data-id": "layout" }).forEach(([name, value]) =>
+      element.setAttribute(name, value)
+    );
+    return element;
+  });
+}
+
 function createElement(type, props) {
   switch (type) {
     case "h1":
